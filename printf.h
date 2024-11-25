009b86decd23683fbae692192c8d228736a6b303
ehossain <ekram.aucarre@gmail.com>
2024-11-25 11:07:56 +0100

    First Commit printf

---
+++ printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 16:27:23 by ehossain          #+#    #+#             */
+/*   Updated: 2024/11/25 10:38:35 by ehossain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
